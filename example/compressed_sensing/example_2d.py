e2b59653304e61f97b20dcb27124488ae5ef7705
khanh <nguyenngockhanh.pbc@gmail.com>
2021-09-03 16:42:17 +0800

    updated

--- example/compressed_sensing/example_2d.py
+++ example/compressed_sensing/example_2d.py
@@ -11,51 +11,67 @@ if __name__ == "__main__":
     ax = ax.flatten()
     ax_index = 0
 
-    width, height, channel = 0, 0, 0
+    height, width, channel = 0, 0, 0
 
 
-    def open(filename: str, num_pixels: int) -> np.ndarray:
+    def open_im(filename: str, num_pixels: int) -> np.ndarray:
         im = Image.open(filename)
         h, w = im.size
         scale = np.sqrt(num_pixels / (h * w))
-        im.thumbnail(size=(int(w * scale), int(h * scale)), resample=Image.ANTIALIAS)
+        if scale < 1:
+            im.thumbnail(size=(int(w * scale), int(h * scale)), resample=Image.ANTIALIAS)
         im = np.array(im)
-        # im = im[:, :, 0]
-        global width, height, channel
-        height, width, channel = im.shape
-        im = im.reshape((height*width, channel))
         return im
 
 
-    def draw(im: np.ndarray, title: str = "im"):
-        im = im.real.astype(np.float64)  # take real part
-        im += 0.5
-        im[im < 0] = 0
-        im[im > 255] = 255
-        im = im.astype(np.uint8)
-        im = im.reshape((height, width, channel))
+    def draw_im(im: np.ndarray, title: str = "im"):
         global ax_index
         ax[ax_index].imshow(im)
         ax[ax_index].set_title(title)
         ax_index += 1
 
 
-    true_signal = open(filename="example_2d.png", num_pixels=6000)
+    def im2sig(im: np.ndarray) -> np.ndarray:
+        signal = im.reshape((height * width, channel))
+        return signal
 
-    draw(true_signal, "true signal")
 
+    def sig2im(signal: np.ndarray) -> np.ndarray:
+        signal = signal.real.astype(np.float64)  # take real part
+        signal += 0.5
+        signal[signal < 0] = 0
+        signal[signal > 255] = 255
+        signal = signal.astype(np.uint8)
+        im = signal.reshape((height, width, channel))
+        return im
+
+    # open true im
+    true_im = open_im(filename="example_2d.png", num_pixels=10000)
+    draw_im(true_im, "true signal")
+    height, width, channel = true_im.shape
+    true_signal = im2sig(true_im)
+
+    # create measure im
     N = len(true_signal)
     D = int(0.3 * N)
     measure_matrix = create_measure_matrix(D, N)
     measure_signal = measure_matrix @ true_signal
+    measure_im = sig2im(measure_matrix.T @ measure_signal)
+    draw_im(measure_im, "measure signal")
 
-    draw(measure_matrix.T @ measure_signal, "measure signal")
+    plt.show()
 
-    reconstruct_signal = np.empty(shape=(height*width, channel), dtype=np.complex128)
+    # reconstruct true im
+    reconstruct_signal = np.empty(shape=(height * width, channel), dtype=np.complex128)
     for c in range(channel):
-        reconstruct_signal[:, c] = reconstruct(measure_signal[:, c], measure_matrix, discrete_fourier_2d.backward(2*height, 2*width, height, width))
+        reconstruct_signal[:, c] = reconstruct(
+            measure_signal=measure_signal[:, c],
+            measure_matrix=measure_matrix,
+            inverse_fourier_matrix=discrete_fourier_2d.backward(2 * height, 2 * width, height, width),
+        )
+    reconstruct_im = sig2im(reconstruct_signal)
+    draw_im(reconstruct_im, "reconstruct signal")
 
-    draw(reconstruct_signal, "reconstruct signal")
-
+    # draw
     plt.tight_layout()
     plt.show()
